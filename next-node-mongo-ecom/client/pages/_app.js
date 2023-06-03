c1764ee7504c2c417d42338e80380977c6a31f2e
Edasgh <edas25564@gmail.com>
2023-06-03 12:31:06 +0530

    "React projects"

--- next-node-mongo-ecom/client/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import mainLayOut from "@/Components/layout/main_LayOut";
-import "@/styles/globals.css";
-import "@/styles/main.scss";
-
-export const myApp=({Component, pageProps})=>{
-    <mainLayOut>
-        <Component {...pageProps}/>
-    </mainLayOut>
-}

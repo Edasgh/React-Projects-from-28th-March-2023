816b7e8f65272dcf22a118b805c438b12364f7b7
Edasgh <edas25564@gmail.com>
2023-07-19 18:02:13 +0530

    rtk-add-to-cart done for now and some resources added

--- from-10th-july/Apps/rtk-add-to-cart/src/Screens/Home.jsx
+++ from-10th-july/Apps/rtk-add-to-cart/src/Screens/Home.jsx
@@ -2,26 +2,26 @@ import React, { useEffect } from "react";
 import Card from "../Components/Card";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../redux/slice(s)/productSlice";
-// import { STATUSES } from "../redux/slice(s)/productSlice";
+import { STATUSES } from "../redux/slice(s)/productSlice";
 import { addToCart, removeFromCart } from "../redux/slice(s)/cartSlice";
-import { shoppingItems } from "../data";
+// import { shoppingItems } from "../data";
 
 const Home = () => {
   const dispatch = useDispatch();
-  // const { data: products, status } = useSelector((state) => state.product);
-  const products = shoppingItems;
+  const { data: products, status } = useSelector((state) => state.product);
+  // const products = shoppingItems;
 
-  // useEffect(() => {
-  //   dispatch(fetchProducts());
-  // }, []);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
 
-  // if (status === STATUSES.LOADING) {
-  //   return <h2>Loading....</h2>;
-  // }
+  if (status === STATUSES.LOADING) {
+    return <h2>Loading....</h2>;
+  }
 
-  // if (status === STATUSES.ERROR) {
-  //   return <h2>Something went wrong!</h2>;
-  // }
+  if (status === STATUSES.ERROR) {
+    return <h2>Something went wrong!</h2>;
+  }
 
   return (
     <>

fc62300dd1227098c7aee8d57b24d9c6acb9ea47
Edasgh <120154741+Edasgh@users.noreply.github.com>
2023-07-27 12:22:55 +0530

    Update appwrite_config.js

--- appwrite-apps-and-demos/apps/Ecom/src/Appwrite/appwrite_config.js
+++ appwrite-apps-and-demos/apps/Ecom/src/Appwrite/appwrite_config.js
@@ -3,10 +3,10 @@ import { Client, Account , Databases ,Storage } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint("http://localhost:8080/v1")
-  .setProject("64757398313495d3c29b");
+  .setEndpoint(process.env.EP)
+  .setProject(process.env.API_KEY);
 
 export const account = new Account(client);
-export const databases = new Databases(client , "647592808a5c92596610")
+export const databases = new Databases(client ,process.env.PRODUCTDB_ID)
 export const storage = new Storage(client)
 // export const databases = new Databases(client , DATABASE_ID)

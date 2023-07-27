a668eb87c303fa187e405fce26247de12be82167
Edasgh <120154741+Edasgh@users.noreply.github.com>
2023-07-27 12:11:24 +0530

    Update DB.js

--- appwrite-apps-and-demos/apps/Ecom/src/api/DB.js
+++ appwrite-apps-and-demos/apps/Ecom/src/api/DB.js
@@ -1,4 +1,5 @@
 const sdk = require('node-appwrite');
+require('dotenv').config();
 
 // Init SDK
 const client = new sdk.Client();
@@ -7,9 +8,9 @@ const ID = sdk.ID
 const databases = new sdk.Databases(client);
 
 client
-    .setEndpoint('http://localhost:8080/v1') // Your API Endpoint
-    .setProject('64757398313495d3c29b') // Your project ID
-    .setKey('dafe61ede30f4e8a409fbbe7d13934cf2214cbadd5b123f38f06a17d30d12df3ac940957c00e0f4628476d5417ace81bbec94d27c9e6a23f4fc8d30aa02258654eadfb04921c6f5587b44dba3af00a25a6ed4d447b0e1f292e582f3b661f6df0be50eb45aa047b0b30c409dfca7f1d0e7ed768d1333d483e70e348428ad9cbcf') // Your secret API key
+    .setEndpoint(process.env.EP) // Your API Endpoint
+    .setProject(process.env.PID) // Your project ID
+    .setKey(process.env.API_KEY) // Your secret API key
 ;
 
 

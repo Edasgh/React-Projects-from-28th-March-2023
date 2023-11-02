8b02e54753477d147b622795d3d12d43f6b8ba74
Edasgh <edas25564@gmail.com>
2023-11-02 17:52:35 +0530

    "Mongodb connection set up"

--- from-10th-july/Apps/MERN-Chat/backend/server.js
+++ from-10th-july/Apps/MERN-Chat/backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const colors = require("colors");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(cors());
+app.use(express.json()); // to accept json data
 
 const port = process.env.PORT;
 
@@ -14,14 +20,8 @@ app.get("/", (req, res) => {
   res.send(`<h1>Welcome to the chat-app server</h1>`);
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
-app.get("/api/chats/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server listening at port no. : ${port}`);
+  console.log(`Server listening at port no. : ${port}`.yellow.bold);
 });

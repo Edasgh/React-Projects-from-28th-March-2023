4b1b74e6458144af6717683f43c790ab0c8958e1
Edasgh <edas25564@gmail.com>
2025-02-15 17:30:55 +0530

    sss

--- from-10th-july/Apps/MERN-movie_ticket_booking/client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div className="App-container"></div>
-    </>
-  );
-}
-
-export default App;
--- from-10th-july/Apps/MERN-movie_ticket_booking/server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { connectDB } = require("./config/db");
-const app = express();
-const port = 8080;
-
-app.listen(port, async () => {
-  await connectDB();
-  console.log(`server listening on http://localhost:${port}`);
-});
